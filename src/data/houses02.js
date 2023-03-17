eea5a36c962e2c3a6cb52576b6aacfd4f95990ea
vasilii <porokh.vs@gmail.com>
2023-03-17 20:57:06 +0300

    process

--- src/data/houses02.js
+++ src/data/houses02.js
@@ -1,13 +1,8 @@
-import * as turf from '@turf/turf'
 
-
-
-
-// var line = turf.lineString([[-83, 30], [-84, 36], [-78, 41]], { "stroke": "#F00" });
-// var offsetLine = turf.lineOffset(line, 2, {units: 'miles'});
-// console.log(offsetLine)
-
-
+import {
+    parallelLine,
+    getLength
+} from '../helpers/geomHelper'
 
 
 let id = 0
@@ -17,12 +12,45 @@ let getID = () => {
 }
 
 
+const breakLineToCut = (p0, p1, w = 800, offset = 'random') => {
+    const len = getLength(...p0, ...p1)
+
+    const normalizedW = w / len
+
+    const phase01 = Math.random() * (1 - normalizedW)
+    const phase02 = phase01 + normalizedW
+
+    const l1 = [
+        [...p0],
+        [p0[0] + (p1[0] - p0[0]) * phase01, p0[1] + (p1[1] - p0[1]) * phase01,],
+    ]
+
+    const l2 = [
+        [p0[0] + (p1[0] - p0[0]) * phase01, p0[1] + (p1[1] - p0[1]) * phase01,],
+        [p0[0] + (p1[0] - p0[0]) * phase02, p0[1] + (p1[1] - p0[1]) * phase02],
+    ]
+
+    const l3 = [
+        [p0[0] + (p1[0] - p0[0]) * phase02, p0[1] + (p1[1] - p0[1]) * phase02],
+        [...p1],
+    ]
+
+    return [l1, l2, l3]
+}
+
+
 const createRoomsPoints = (center) => {
+    const rooms = []
     const D_MAX = 5000
     const D_MIN = 2000
 
     const roomId = getID()
     const sTartPoint = [-D_MIN - Math.random() * D_MAX + center[0], D_MIN + Math.random() * D_MAX + center[1]]
+
+
+
+    const lll = breakLineToCut([0, 0], [10000, 0], 600)
+    console.log('---',lll)
 
 
     const path = [
@@ -32,15 +60,10 @@ const createRoomsPoints = (center) => {
         [-D_MIN - Math.random() * D_MAX + center[0], -D_MIN - Math.random() * D_MAX + center[1]],
         [...sTartPoint],
     ]
-    const line = turf.lineString(path)
-    const offsetLine = turf.lineOffset(line, 300000, {units: 'miles'});
-
-    console.log(path)
-    console.log(offsetLine)
-
 
     const mh0 = 0
 
+    /** central room */
     const room = {
         id: roomId,
         mh0,
@@ -50,6 +73,7 @@ const createRoomsPoints = (center) => {
         ],
         "class": "bedroom",
     }
+
 
     const innerPerimeter = []
     for (let i = 1; i < path.length; ++i) {
@@ -67,9 +91,58 @@ const createRoomsPoints = (center) => {
         })
     }
 
-    return {
+    rooms.push({
         room,
         innerPerimeter,
+    })
+
+    /** top room *****/
+    const l = parallelLine([...path[2], ...path[3],], 300)
+    const pathTop = [
+        [l[2], l[3]],
+        [l[0], l[1]],
+        [l[0], l[1] - D_MIN - Math.random() * D_MAX * 3],
+        [l[2], l[3] - D_MIN - Math.random() * D_MAX * 3],
+        [l[2], l[3]],
+    ]
+
+    const roomId_02 = getID()
+    const room_02 = {
+        id: roomId_02,
+        mh0,
+        path: pathTop,
+        parameters: [
+            { height : 2900 },
+        ],
+        "class": "bedroom",
+    }
+
+    const innerPerimeter02 = []
+    for (let i = 1; i < pathTop.length; ++i) {
+        innerPerimeter02.push({
+            mh0,
+            "id": getID(),
+            "class": "inner-wall",
+            "type": "solid",
+            "ref-room": roomId,
+            "path":
+                [
+                    [...pathTop[i]],
+                    [...pathTop[i - 1]],
+                ]
+        })
+    }
+
+
+
+    rooms.push({
+        room: room_02,
+        innerPerimeter: innerPerimeter02,
+    })
+
+
+    return {
+        rooms,
     }
 }
 
@@ -78,9 +151,12 @@ const createRoomsPoints = (center) => {
 const rooms = []
 const innerPerimeter = []
 for (let i = 0; i < 5; ++i) {
-    const result = createRoomsPoints([i * 15000, 0])
-    rooms.push(result.room)
-    innerPerimeter.push(...result.innerPerimeter)
+    const result = createRoomsPoints([i * 30000, 0])
+    for (let i = 0; i < result.rooms.length; ++i) {
+        rooms.push(result.rooms[i].room)
+        result.rooms[i].innerPerimeter && innerPerimeter.push(...result.rooms[i].innerPerimeter)
+    }
+
 }
 
 
@@ -90,358 +166,3 @@ export const flatJSON = {
     "inner-perimeters": [innerPerimeter],
     "objects": [[]]
 }
-
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// const rooms = []
-// const innerPerimeters = []
-// const objects = []
-//
-// let mh0 = 0
-//
-// let currentZ = -4000
-// let newZ = 4000
-//
-//
-//
-// const createFloor = () => {
-//     let currentX = -20000
-//
-//     let startDoorZ = currentZ + 1000 + (Math.random() * (newZ - currentZ - 2000))
-//     let endDoorZ = startDoorZ + 800
-//
-//     for (let i = 0; i < 3; ++i) {
-//         const w = Math.random() * 7000 + 3000
-//         const newX = currentX + w
-//
-//         const roomId = getID()
-//         rooms.push({
-//             mh0,
-//             "id": roomId,
-//             "class": "bedroom",
-//             'path': [
-//                 [currentX, currentZ],
-//                 [currentX, newZ],
-//                 [newX, newZ],
-//                 [newX, currentZ],
-//                 [currentX, currentZ],
-//             ],
-//             "parameters": [
-//                 {
-//                     "height": 2900,
-//                     "wall-texture": "GUID-iurn9rewqer0g",
-//                     "floor-texture": "GUID-iufsdhfdhfqer0g",
-//                     "ceiling-texture": "GUID-iurn9rgsdfhxvcng"
-//                 }
-//             ],
-//         })
-//
-//
-//         let newStartDoorZ = currentZ + 1000 + (Math.random() * (newZ - currentZ - 2000))
-//         let newEndDoorZ = newStartDoorZ + 800
-//
-//         const doorWallId = getID()
-//         const doorWallId_02 = getID()
-//         const windowWallId = getID()
-//         const windowWallId02 = getID()
-//
-//         const windowX01 = currentX + Math.random() * (newX - currentX) * 0.3 + 500
-//         const windowX02 = windowX01 + Math.random() * (newX - windowX01) * 0.8 + 1000
-//
-//         innerPerimeters.push(
-//             /** TOP */
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [currentX, currentZ],
-//                         [windowX01, currentZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [windowX02, currentZ],
-//                         [newX, currentZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": windowWallId,
-//                 "class": "inner-wall",
-//                 "type": "cut",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [windowX01, currentZ],
-//                         [windowX02, currentZ],
-//                     ]
-//             },
-//
-//             /** BOTTOM */
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [newX, newZ],
-//                         [windowX02, newZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": windowWallId02,
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [windowX02, newZ],
-//                         [windowX01, newZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "cut",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [windowX01, newZ],
-//                         [currentX, newZ],
-//                     ]
-//             },
-//
-//
-//             /** LEFT */
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [currentX, startDoorZ],
-//                         [currentX, currentZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": doorWallId,
-//                 "class": "inner-wall",
-//                 "type": "cut",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [currentX, endDoorZ],
-//                         [currentX, startDoorZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [currentX, newZ],
-//                         [currentX, endDoorZ],
-//                     ]
-//             },
-//             /** RIGHT */
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [newX, currentZ],
-//                         [newX, newStartDoorZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": doorWallId_02,
-//                 "class": "inner-wall",
-//                 "type": "cut",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [newX, newStartDoorZ],
-//                         [newX, newEndDoorZ],
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "inner-wall",
-//                 "type": "solid",
-//                 "ref-room": roomId,
-//                 "path":
-//                     [
-//                         [newX, newEndDoorZ],
-//                         [newX, newZ],
-//                     ]
-//             },
-//         )
-//
-//         objects.push(
-//             {
-//                 mh0,
-//                 "id": "349",
-//                 "class": "door",
-//                 "parameters": [{
-//                     "type": "interior-door",
-//                     "height-bottom": 0,
-//                     "height-top": 2000
-//                 }],
-//                 "location": [{
-//                     "ref-wall": doorWallId_02,
-//                     "path": [
-//                         [currentX - 200, startDoorZ],
-//                         [currentX - 200, endDoorZ],
-//                     ]
-//                 },
-//                     {
-//                         "ref-wall": doorWallId,
-//                         "path": [
-//                             [currentX, endDoorZ],
-//                             [currentX, startDoorZ],
-//                         ]
-//                     }
-//                 ]
-//             },
-//             {
-//                 mh0,
-//                 "id": "347",
-//                 "class": "window",
-//                 "parameters":
-//                     [
-//                         {
-//                             "height-bottom": 960,
-//                             "height-top": 2660
-//                         }
-//                     ],
-//                 "location":
-//                     [
-//                         {
-//                             "ref-wall": windowWallId,
-//                             "path":
-//                                 [
-//                                     [windowX02, currentZ],
-//                                     [windowX01, currentZ]
-//                                 ]
-//                         },
-//                         {
-//                             "ref-wall": "232",
-//                             "path":
-//                                 [
-//                                     [windowX02, currentZ - 200],
-//                                     [windowX01, currentZ - 200]
-//                                 ]
-//                         },
-//
-//                     ]
-//             },
-//             {
-//                 mh0,
-//                 "id": getID(),
-//                 "class": "window",
-//                 "parameters":
-//                     [
-//                         {
-//                             "height-bottom": 960,
-//                             "height-top": 2660
-//                         }
-//                     ],
-//                 "location":
-//                     [
-//                         {
-//                             "ref-wall": windowWallId02,
-//                             "path":
-//                                 [
-//                                     [windowX01, newZ],
-//                                     [windowX02, newZ],
-//
-//                                 ]
-//                         },
-//                         {
-//                             "ref-wall": "232",
-//                             "path":
-//                                 [
-//                                     [windowX01, newZ - 200],
-//                                     [windowX02, newZ - 200],
-//
-//                                 ]
-//                         },
-//
-//
-//                     ]
-//             },
-//         )
-//
-//         startDoorZ = newStartDoorZ
-//         endDoorZ = newEndDoorZ
-//         currentX = newX + 200
-//
-//     }
-// }
-//
-// /** change Z **/
-// for (let j = 0; j < 3; ++j) {
-//     mh0 = 0
-//     currentZ -= 20000
-//     newZ = currentZ + 10000
-//
-//     /** change H **/
-//     const H = Math.floor(Math.random() * 7) + 4
-//     for (let i = 0; i < H; ++i) {
-//         createFloor()
-//         mh0 += 3000
-//     }
-// }
-//
-//
-// export const flatJSON = (() => {
-//     const data = {
-//         "rooms": [rooms],
-//         "outer-perimeter":
-//             [
-//                 []
-//             ],
-//         "inner-perimeters": [ innerPerimeters ],
-//         "objects": [objects]
-//     }
-//
-//     return data
-// })()
--- src/helpers/geomHelper.js
+++ src/helpers/geomHelper.js
@@ -2,12 +2,7 @@ import { m4 } from './m4'
 
 export const createFace4 = (v1, v2, v3, v4) => [...v1, ...v2, ...v3, ...v1, ...v3, ...v4]
 
-export const angleFromCoords = (x, y) => {
-    let rad = Math.atan(y / x)
-    x < 0 && y > 0 && (rad = Math.PI - Math.abs(rad))
-    x < 0 && y <= 0 && (rad = Math.PI + Math.abs(rad))
-    return rad
-}
+
 
 export const rotateArrY = (arr, angle) => {
     const matrix = m4.yRotation(angle);
@@ -41,4 +36,41 @@ export const translateArr = (arr, x = 0, y = 0, z = 0) => {
         arr[i + 1] = vector[1]
         arr[i + 2] = vector[2]
     }
+}
+
+
+
+export const angleFromCoords = (x, y) => {
+    let rad = Math.atan(y / x)
+    x < 0 && y > 0 && (rad = Math.PI - Math.abs(rad))
+    x < 0 && y <= 0 && (rad = Math.PI + Math.abs(rad))
+    return rad
+}
+
+export const angleOfLine = (x0, z0, x1, z1) => {
+    const dX = x1 - x0
+    const dZ = z1 - z0
+    const angle = angleFromCoords(dX, dZ)
+    return angle
+}
+
+export const getLength = (x0, y0, x1, y1) => {
+    const dX = x1 - x0
+    const dY = y1 - y0
+    return Math.sqrt(dX * dX + dY * dY)
+}
+
+
+export const parallelLine = (arr, offset) => {
+    const l = getLength(...arr)
+    const angle = angleOfLine(...arr)
+
+    const newLine = [0, 11111, 0, l, 1111, 0]
+    rotateArrY(newLine, -angle)
+
+    const newX = arr[0] + Math.sin(-angle) * offset
+    const newZ = arr[1] + Math.cos(-angle) * offset
+
+    translateArr(newLine, newX, 0, newZ)
+    return [newLine[0], newLine[2], newLine[3], newLine[5]]
 }
