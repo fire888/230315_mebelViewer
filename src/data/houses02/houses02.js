d2220e8655254ec0a306100e22ba0c409909fc62
vasilii <porokh.vs@gmail.com>
2023-03-20 16:06:43 +0300

    process

--- src/data/houses02/houses02.js
+++ src/data/houses02/houses02.js
@@ -11,151 +11,54 @@ const W = 5000
 
 
 const createFloor = (root, houseIndex, floorIndex, bottomRooms) => {
-    const arrGenerate = []
     const arrLines = []
 
-    const createLine = (z) => {
+    const createLine = (indexLine) => {
         const arrLine = []
 
         for (let i = 0; i < 5; ++i) {
-            //const nWall = null
+
+            let pp = {}
+            if (
+                bottomRooms &&
+                bottomRooms[indexLine] &&
+                bottomRooms[indexLine][i]
+            ) {
+                const bottomRoom = bottomRooms[indexLine][i]
+                pp = {
+                    nw: bottomRoom.nw,
+                    ne: bottomRoom.ne,
+                    se: bottomRoom.se,
+                    sw: bottomRoom.sw,
+                }
+            }
+
             const nWall = (arrLines[arrLines.length - 1] && arrLines[arrLines.length - 1][i]) ? arrLines[arrLines.length - 1][i].sWall : null
             const eWall = arrLine[arrLine.length - 1] ? arrLine[arrLine.length - 1].wWall : null
             const r = new Room(
                  root,
-                 [houseIndex * step - (W * i), z],
+                 [houseIndex * step - (W * i), indexLine * W],
                 {
                     nWall,
                     eWall,
                 },
-                 H * floorIndex,
-                {},
-                //pp
+                floorIndex * H,
+                pp,
             )
             arrLine.push(r)
-            arrGenerate.push(r)
         }
 
         return arrLine
     }
 
     for (let i = 0; i < 5; ++i) {
-        const line = createLine(i * W)
+        const line = createLine(i)
         arrLines.push(line)
     }
 
 
-    // const pp = bottomRooms
-    //     ? {
-    //         sw: bottomRooms[0].sw,
-    //         nw: bottomRooms[0].nw,
-    //         ne: bottomRooms[0].ne,
-    //         se: bottomRooms[0].se,
-    //     } : {}
-    //
-    // const r = new Room(
-    //     root,
-    //     [houseIndex * step, 0],
-    //     {},
-    //     H * floorIndex,
-    //     pp
-    // )
-    // arr.push(r)
-    //
-    // const rW = new Room(
-    //      root,
-    //      [houseIndex * step - W, 0],
-    //     {
-    //         eWall: r.wWall,
-    //     },
-    //      H * floorIndex,
-    //      pp
-    // )
-    // arr.push(rW)
-    //
-    // const rS = new Room(
-    //     root,
-    //     [houseIndex * step, W],
-    //     {
-    //         nWall: r.sWall,
-    //     },
-    //     H * floorIndex,
-    //     pp
-    // )
-    // arr.push(rS)
-    //
-    // const rWS = new Room(
-    //     root,
-    //     [houseIndex * step - W, W],
-    //     {
-    //         nWall: rW.sWall,
-    //         eWall: rS.wWall,
-    //     },
-    //     H * floorIndex,
-    //     pp
-    // )
-    // arr.push(rWS)
-    //
-    // const rWSS = new Room(
-    //     root,
-    //     [houseIndex * step - W, W * 2],
-    //     {
-    //         nWall: rWS.sWall,
-    //     },
-    //     H * floorIndex,
-    //     pp
-    // )
-    // arr.push(rWSS)
-    //
-    // const rWSW = new Room(
-    //     root,
-    //     [houseIndex * step - W * 2, W],
-    //     {
-    //         eWall: rWS.wWall,
-    //     },
-    //     H * floorIndex,
-    //     pp
-    // )
-    // arr.push(rWSW)
-    //
-    // const rWSWS = new Room(
-    //     root,
-    //     [houseIndex * step - W * 2, W * 2],
-    //     {
-    //         eWall: rWSS.wWall,
-    //         nWall: rWSW.sWall,
-    //     },
-    //     H * floorIndex,
-    //     pp
-    // )
-    // arr.push(rWSWS)
 
-
-
-
-
-    // const fullNumSouthRoomsRandom = Math.floor(Math.random() * 3) + 1
-    //
-    // const fullNumSouthRooms = bottomRooms ? bottomRooms.length : fullNumSouthRoomsRandom
-    // for (let indexRoomS = 1; indexRoomS < fullNumSouthRooms; ++indexRoomS) {
-    //     const pp = bottomRooms
-    //     ? {
-    //         sw: bottomRooms[indexRoomS].sw,
-    //         nw: bottomRooms[indexRoomS].nw,
-    //         ne: bottomRooms[indexRoomS].ne,
-    //         se: bottomRooms[indexRoomS].se,
-    //     } : {}
-    //
-    //     const rBottom = new Room(
-    //         root,
-    //         [houseIndex * step, indexRoomS * 7000],
-    //         { nWall: arr[arr.length - 1].sWall },
-    //         H * floorIndex,
-    //         pp
-    //     )
-    //     arr.push(rBottom)
-    // }
-    return arrGenerate
+    return arrLines
 }
 
 
@@ -164,7 +67,8 @@ const createDom = (root, houseIndex) => {
     const arr = []
 
     //const floorsNum = Math.floor(Math.random() * 10) + 5
-    const floorsNum = 1//Math.floor(Math.random() * 10) + 5
+    const floorsNum = 3//Math.floor(Math.random() * 10) + 5
+
 
     for (let floorIndex = 0; floorIndex < floorsNum; ++floorIndex) {
         const prevArr = floorIndex === 0 ? null : arr[floorIndex - 1]
@@ -188,7 +92,9 @@ export const createPerimeters = (root) => {
     for (let i = 0; i < arr.length; ++i) {
         for (let j = 0; j < arr[i].length; ++j) {
             for (let k = 0; k < arr[i][j].length; ++k) {
-                arr[i][j][k].generateMeshes()
+                for (let l = 0; l < arr[i][j][k].length; ++l) {
+                    arr[i][j][k][l].generateMeshes()
+                }
             }
         }
     }
